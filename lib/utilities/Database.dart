0ec0722b1714d5279b188132f643925ce07eb976
= <=>
2021-04-08 19:03:07 -0400

    SQLite backend mostly finished, integrating is next

--- lib/utilities/Database.dart
+++ lib/utilities/Database.dart
@@ -2,26 +2,26 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 //import 'todo.dart';
 
-/// Handles SQLite database functions
+/// Handles SQLite database functions.
 class DatabaseHelper {
 
-  // Database and table constants
-  static final _databaseName = "flip.db";
+  // Database and table constants.
+  static const _databaseName = "flip.db";
   static final _databaseVersion = 1;
   static Database _database;
 
-  static final musicSongTable = 'music_song';
-  static final musicInstrumentTable = 'music_instrument';
-  static final musicFileTable = 'music_file';
+  static const musicSongTable = 'music_song';
+  static const musicInstrumentTable = 'music_instrument';
+  static const musicFileTable = 'music_file';
 
-  static final drillShowTable = 'drill_show';
-  static final drillFileTable = 'drill_file';
+  static const drillShowTable = 'drill_show';
+  static const drillFileTable = 'drill_file';
 
 
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
 
-  /// Grab the database if it exists or initialize it if it doesn't
+  /// Grab the database if it exists or initialize it if it doesn't.
   Future<Database> get database async {
     if (_database != null) {
       return _database;
@@ -32,20 +32,20 @@ class DatabaseHelper {
   }
 
   /// Initialize the database by grabbing the path if it exists or creating
-  /// one it if it doesn't
+  /// one it if it doesn't.
   _initDatabase() async {
     String path = join(await getDatabasesPath(), _databaseName);
     return await openDatabase(path, version: _databaseVersion,
         onCreate: _onCreate, onConfigure: _onConfigure);
   }
 
-  /// Executes SQL code to configure the database
+  /// Executes SQL code to configure the database.
   Future _onConfigure(Database db) async {
-    // Enable foreign key support (REFERENCES keyword in table creation)
+    // Enable foreign key support (REFERENCES keyword in table creation).
     await db.execute('PRAGMA foreign_keys = ON');
   }
 
-  /// Executes SQL code to create all tables needed in the database
+  /// Executes SQL code to create all tables needed in the database.
   Future _onCreate(Database db, int version) async {
     await db.execute('''
           BEGIN TRANSACTION;
@@ -84,27 +84,89 @@ class DatabaseHelper {
           ''');
   }
 
-  /// Insert provided data into the specified table
+  /// Return the primary key column name for the table provided.
+  String _determinePrimaryKeyColumnName(String table) {
+    switch(table) {
+      case musicSongTable: {
+        return "music_song_id";
+      }
+      case musicInstrumentTable: {
+        return "music_instrument_id";
+      }
+      case musicFileTable: {
+        return "music_file_id";
+      }
+      case drillShowTable: {
+        return "drill_show_id";
+      }
+      case drillFileTable: {
+        return "drill_file_id";
+      }
+      default: {
+        return "NULL";
+      }
+    }
+  }
+
+  /// Return the primary key column name for the table provided.
+  String _determineTextColumnName(String table) {
+    switch(table) {
+      case musicSongTable: {
+        return "music_song_name";
+      }
+      case musicInstrumentTable: {
+        return "music_instrument_name";
+      }
+      case musicFileTable: {
+        return "music_file_name";
+      }
+      case drillShowTable: {
+        return "drill_show_name";
+      }
+      case drillFileTable: {
+        return "drill_file_name";
+      }
+      default: {
+        return "NULL";
+      }
+    }
+  }
+
+  /// Insert provided data into the specified table.
   Future<int> insert(String table, Map<String, dynamic> data) async {
     Database db = await instance.database;
     var id = await db.insert(table, data, conflictAlgorithm: ConflictAlgorithm.replace);
     return id;
   }
 
+  /// Delete provided row id in the specified table.
   Future<int> delete(String table, int id) async {
     Database db = await instance.database;
-    var numDeleted = await db.delete(table, where: '$columnId = ?', whereArgs: [id]);
+    String primaryKey = _determinePrimaryKeyColumnName(table);
+    var numDeleted = await db.delete(table, where: '$primaryKey = ?', whereArgs: [id]);
     return numDeleted;
   }
 
-  Future<List<Map<String, dynamic>>> queryAllRows() async {
+  /// Query a specified table to get a list ordered by the primary key.
+  Future<List<Map<String, dynamic>>> query(String table) async {
     Database db = await instance.database;
-    var res = await db.query(table, orderBy: "$columnId DESC");
-    return res;
+    String primaryKey = _determinePrimaryKeyColumnName(table);
+    var items = await db.query(table, orderBy: "$primaryKey");
+    return items;
   }
-  
-  Future<void> clearTable() async {
+
+  /// Search a specified table to get a list.
+  Future<List<Map<String, dynamic>>> search(String table, String keywords) async {
     Database db = await instance.database;
-    return await db.rawQuery("DELETE FROM $table");
+    String name = _determineTextColumnName(table);
+    var items = await db.query(table, where: '$name = ?', whereArgs: [keywords]);
+    return items;
+  }
+
+  /// Clear an entire table.
+  Future<void> clearTable(String table) async {
+    Database db = await instance.database;
+    var rows = await db.rawQuery("DELETE FROM $table");
+    return rows;
   }
 }

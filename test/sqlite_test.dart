18ad7ec3a85c9ccafb58369354ef410a80de9517
= <=>
2021-04-10 22:07:26 -0500

    finished tests but getting weird error

--- test/sqlite_test.dart
+++ test/sqlite_test.dart
@@ -3,13 +3,15 @@ import 'package:flutter_test/flutter_test.dart';
 
 /// Test the SQLite functions in Database.dart to ensure backend functions as expected.
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   test('test inserting into database', () async {
     Map<String, dynamic> name = {
       FlipDatabase.instance.determineTextColumnName(FlipDatabase.musicSongTable)
           : 'Dynamite'
     };
 
-    int row = await FlipDatabase.instance.insert(FlipDatabase.musicFileTable, name);
+    int row = await FlipDatabase.instance.insert(FlipDatabase.musicSongTable, name);
     expect(row, 0);
 
     name = {
@@ -17,7 +19,7 @@ void main() {
           : 'Anchors Aweigh'
     };
 
-    row = await FlipDatabase.instance.insert(FlipDatabase.musicFileTable, name);
+    row = await FlipDatabase.instance.insert(FlipDatabase.musicSongTable, name);
     expect(row, 1);
   });
 
@@ -27,10 +29,44 @@ void main() {
           : 'Dynamite'
     };
 
-    int row = await FlipDatabase.instance.delete(FlipDatabase.musicFileTable, name);
+    List<Map<String, dynamic>> row = await FlipDatabase.instance.search(
+        FlipDatabase.musicSongTable, "Dynamite");
+    List<Map<String, dynamic>> answer = [
+      name
+    ];
+
+    expect(row, answer);
+  });
+
+  test('test querying from database', () async {
+    Map<String, dynamic> name = {
+      FlipDatabase.instance.determineTextColumnName(FlipDatabase.musicSongTable)
+          : 'Dynamite'
+    };
+
+    Map<String, dynamic> name2 = {
+      FlipDatabase.instance.determineTextColumnName(FlipDatabase.musicSongTable)
+          : 'Anchors Aweigh'
+    };
+
+    List<Map<String, dynamic>> row = await FlipDatabase.instance.query(
+        FlipDatabase.musicSongTable);
+
+    List<Map<String, dynamic>> answer = [
+      name,
+      name2
+    ];
+
+    expect(row, answer);
+  });
+
+  test('test deleting from database', () async {
+    int row = await FlipDatabase.instance.delete(FlipDatabase.musicSongTable, 0);
     expect(row, 0);
   });
 
-  // Clear database after testing is complete
-  FlipDatabase.instance.clearDatabase();
+  test('test clearing database', () async {
+    await FlipDatabase.instance.clearDatabase();
+  });
+
 }
